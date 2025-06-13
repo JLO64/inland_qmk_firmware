5fa58148263f9db06df84f9c513439490da6d0c1
Julian Lopez <64julianlopez@gmail.com>
2025-06-12 19:44:50 -0700

    first llm keymap attempt

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -17,29 +17,60 @@
 #include QMK_KEYBOARD_H
 
 // clang-format off
+
+/*
+ * Keymap derived from the "mysetup" VIA configuration stored in
+ * mk47.layout.json.  The physical keyboard uses the Planck MIT (47-key)
+ * layout macro, which skips matrix position [3,6] (the unused switch
+ * under the 2u spacebar).  In the VIA file that position is filled with
+ * KC_NO; here it is simply omitted to match the 47-key macro.
+ */
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+    /* Layer 0: Base ------------------------------------------------------------------ */
     [0] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-        KC_LCTL, MO(2),   KC_LALT, KC_DEL,  KC_PGDN,     KC_SPC,       KC_PGUP, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        /* Row 0 */ KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+        /* Row 1 */ KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        /* Row 2 */ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
+        /* Row 3 */
+            KC_GRV, KC_LCTL, KC_LALT, KC_LGUI, TO(2), LT(1, KC_SPC),
+            /* --- matrix [3,6] is skipped by the MIT layout macro --- */
+            KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_ENT
     ),
 
+    /* Layer 1: Numpad / Media --------------------------------------------------------- */
     [1] = LAYOUT_planck_mit(
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RM_NEXT,
-        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, RM_VALU, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, RM_VALD, KC_TRNS
+        /* Row 0 */ KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,    KC_NO,
+        /* Row 1 */ KC_NO,   S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_NO,
+        /* Row 2 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        /* Row 3 */
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            /* skip [3,6] */
+            KC_VOLD, KC_VOLU, RGB_VAD, RGB_VAI, KC_NO
     ),
 
+    /* Layer 2: Navigation ------------------------------------------------------------- */
     [2] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-        KC_LCTL, KC_NO,   KC_LALT, KC_DEL,  KC_PGDN,     KC_SPC,       KC_PGUP, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT
+        /* Row 0 */ TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        /* Row 1 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,    KC_NO,
+        /* Row 2 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        /* Row 3 */
+            KC_NO, KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO,
+            /* skip [3,6] */
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    ),
+
+    /* Layer 3: Spare (mostly empty) --------------------------------------------------- */
+    [3] = LAYOUT_planck_mit(
+        /* Row 0 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        /* Row 1 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,    KC_NO,
+        /* Row 2 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        /* Row 3 */
+            KC_NO, KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO,
+            /* skip [3,6] */
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     )
 
 };
 // clang-format on
-
