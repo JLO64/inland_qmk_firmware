68225d64d56970e6e865b8485142555d65d26dba
Julian Lopez <64julianlopez@gmail.com>
2025-11-08 14:36:05 -0800

    Layer1 now emits standard KC1–KC0 instead of keypad codes

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Layer 1: Numpad / Media --------------------------------------------------------- */
     [1] = LAYOUT_planck_mit(
-        /* Row 0 */ KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,    KC_TRNS,
+        /* Row 0 */ KC_TRNS, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_TRNS,
         /* Row 1 */ KC_TRNS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_TRNS,
         /* Row 2 */ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         /* Row 3 */
